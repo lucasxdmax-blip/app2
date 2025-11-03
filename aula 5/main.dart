07bc60dc8a0092bde0279c80f6c7b2d80964ae0f
Lucas Santos Azevedo <lucasxdmax@gmail.com>
2025-11-03 11:49:54 -0300

    Initialize main.dart for Aula 5 Flutter app

---
+++ aula 5/main.dart
@@ -0,0 +1,24 @@
+import 'package:aula5/acreens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Aula 5',
+      theme: ThemeData(
+       
+        colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 255, 2, 2)),
+      ),
+      home:MyHome() ,
+    );
+  }
+}
+
