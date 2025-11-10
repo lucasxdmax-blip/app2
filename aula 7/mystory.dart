767b7610a907811b91af248e767ec605c74948ca
Lucas Santos Azevedo <lucasxdmax@gmail.com>
2025-11-10 11:56:03 -0300

    Add Mystory widget to display title and avatar

---
+++ aula 7/mystory.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Mystory extends StatelessWidget {
+ final String? title;
+  const Mystory({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        CircleAvatar(
+          radius: 40,
+          backgroundColor: const Color.fromARGB(155, 55, 35, 231),
+          child: FlutterLogo(size: 30,),
+        ),
+        Text(title!)
+      ],
+    );
+  }
+}
