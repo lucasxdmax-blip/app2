d6411146cbbf8f70feeec0e389c78aa274fd22cc
Lucas Santos Azevedo <lucasxdmax@gmail.com>
2025-11-17 11:44:17 -0300

    Implement MybottomNavigation widget with BottomNavigationBar

---
+++ aula 9/mybottomnavigation.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MybottomNavigation extends StatefulWidget {
+  const MybottomNavigation({super.key});
+
+  @override
+  State<MybottomNavigation> createState() => _MybottomNavigationState();
+}
+
+class _MybottomNavigationState extends State<MybottomNavigation> {
+  int _index = 0;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      selectedItemColor: const Color.fromARGB(232, 221, 168, 168),
+      unselectedItemColor: const Color.fromARGB(255, 255, 17, 0),
+      onTap: (i) {
+         setState(() {
+           _index = i;
+         });
+        },
+      currentIndex: _index,
+    items: [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.mail,color: const Color.fromARGB(255, 0, 255, 157),),
+        label: "contatos"
+        ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.settings,color: const Color.fromARGB(255, 0, 255, 157),),
+        label: "configuraçãos",
+        ),
+    ]
+    );
+  }
+}
